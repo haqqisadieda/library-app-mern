5968dd3be121929c8ca74392e268baa406da1e48
haqqisadieda <haqqisadiedapandega@gmail.com>
2022-06-08 13:09:43 +0700

    membuat controller untuk admin

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,54 @@
+import bcrypt from 'bcrypt';
+import Admin from '../models/adminModel.js';
+
+const getAllAdmin = (req, res) => {
+    Admin.find({}, (err, data) => {
+        if (err) return res.status(500).send(err);
+        res.status(200).json(data);
+    });
+};
+
+const adminRegister = (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    Admin.find({ email }, async (err, data) => {
+        if (err) return res.status(500).send(err);
+        if (data.length === 0) {
+            const adminId = data.length + 1;
+            const hashedPwd = await bcrypt.hash(password, 10);
+
+            const dataAdmin = new Admin({
+                adminId,
+                email,
+                password: hashedPwd,
+            });
+
+            dataAdmin.save((err, data) => {
+                if (err) return res.status(500).send(err);
+                res.status(201).json(data);
+            });
+        } else {
+            res.status(409).json({ error: 'Email Already Registered' });
+        }
+    });
+};
+
+const adminLogin = (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    Admin.find({ email }, async (err, data) => {
+        if (err) return res.status(500).send(err);
+        if (data.length === 0) {
+            res.status(204).json({ error: 'Email Not Registered!' });
+        } else {
+            const match = await bcrypt.compare(password, data[0].password);
+            if (!match)
+                return res.status(401).json({ error: 'Wrong Password!' });
+            res.status(202).json({ data });
+        }
+    });
+};
+
+export { getAllAdmin, adminRegister, adminLogin };
--- backend/routes/api/admin.js
+++ backend/routes/api/admin.js
@@ -1,57 +1,16 @@
 import express from 'express';
-import bcrypt from 'bcrypt';
-import Admin from '../../models/adminModel.js';
+import {
+    getAllAdmin,
+    adminRegister,
+    adminLogin,
+} from '../../controllers/adminController.js';
 
 const router = express.Router();
 
-router.route('/').get((req, res) => {
-    Admin.find({}, (err, data) => {
-        if (err) return res.status(500).send(err);
-        res.status(200).json(data);
-    });
-});
+router.route('/').get(getAllAdmin);
 
-router.route('/register').post(async (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
+router.route('/register').post(adminRegister);
 
-    Admin.find({ email }, async (err, data) => {
-        if (err) return res.status(500).send(err);
-        if (data.length === 0) {
-            const adminId = data.length + 1;
-            const hashedPwd = await bcrypt.hash(password, 10);
-
-            const dataAdmin = new Admin({
-                adminId,
-                email,
-                password: hashedPwd,
-            });
-
-            dataAdmin.save((err, data) => {
-                if (err) return res.status(500).send(err);
-                res.status(201).json(data);
-            });
-        } else {
-            res.status(409).json({ error: 'Email Already Registered' });
-        }
-    });
-});
-
-router.route('/login').post((req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-
-    Admin.find({ email }, async (err, data) => {
-        if (err) return res.status(500).send(err);
-        if (data.length === 0) {
-            res.status(204).json({ error: 'Email Not Registered!' });
-        } else {
-            const match = await bcrypt.compare(password, data[0].password);
-            if (!match)
-                return res.status(401).json({ error: 'Wrong Password!' });
-            res.status(202).json({ data });
-        }
-    });
-});
+router.route('/login').post(adminLogin);
 
 export default router;
